2199121c9cb0cf6a3cb904a45ebd4a48ce4526b5
Gustavo de los Campos <gdeloscampos@gmail.com>
2015-04-25 10:49:12 -0400

    Update readGBS.r
    Added posiblity of reading recoded data

--- readGBS.r
+++ readGBS.r
@@ -1,25 +1,43 @@
 # A function to read and recode GBS data (assuming input file with format commonly used by J. Poland's lab).
 # returns a BGData object
 # contact: gdeloscampos@gmail.com
-
-readGBS.AGCT<-function( fileIn, rsCol,allelesCol,nColSkip,header=TRUE,
-                   class='matrix',n=NULL,p=NULL,na.strings='N',returnData=TRUE,
+readGBS<-function( fileIn,nColSkip,recode,header=TRUE,rsCol,sep,
+                   class='matrix',n=NULL,p=NULL,na.strings='NA',returnData=TRUE,
                    verbose=TRUE,nChunks=NULL,vmode='byte',dimorder=1:2,
-                   folderOut=paste('BGData_',sub("\\.[[:alnum:]]+$","",
-                                   basename(fileIn)),sep='') 
+                   folderOut=paste('BGData_',sub("\\.[[:alnum:]]+$","",basename(fileIn)),sep='') 
                   ){
     ########
     # Reads and recode GBS data
     # Value: a BGData object with slots @map and @geno
     ########
+    #setwd('~/Dropbox/wheat_gbs/genos/')
+
+    #fileIn="G80_NonRedund2.csv"
+    #nColSkip=1
+    #header=TRUE
+    #class='matrix'
+    #n=NULL
+    #p=NULL
+    #na.strings='NA'
+    #returnData=TRUE
+    #verbose=TRUE
+    #nChunks=NULL
+    #vmode='byte'
+    #dimorder=1:2
+    #folderOut=paste('BGData_',sub("\\.[[:alnum:]]+$","",basename(fileIn)),sep='') 
+    #sep=','
+    #recode=FALSE
+    #rsCol=1
+    
     if(is.null(n)){
-        tmp<-scan(gzfile(fileIn),nlines=1,what=character(),quiet=T)
+        tmp<-scan(gzfile(fileIn),nlines=1,what=character(),quiet=T,sep=sep)
         n<-length(tmp)-nColSkip
     }
+    
     if(is.null(p)){
        tmp<-gzfile(fileIn,open='r')
        p <- 0
-       while(length(scan(tmp,what=character(),nlines=1,quiet=TRUE))>0){
+       while(length(scan(tmp,what=character(),sep=sep,nlines=1,quiet=TRUE))>0){
             p<-p+1
             if(verbose){
               cat('Determining number of markers (reading line ',p, ')\n')
@@ -29,8 +47,7 @@ readGBS.AGCT<-function( fileIn, rsCol,allelesCol,nColSkip,header=TRUE,
        close(tmp)
      }    
      if(header){
-        pedFile<-gzfile(fileIn,open='r')
-        tmp<-scan(pedFile,nlines=1,what=character(),quiet=TRUE)
+        tmp<-scan(fileIn,nlines=1,what=character(),quiet=TRUE,sep=sep)
         GID<-tmp[-(1:nColSkip)]
     }else{ GID<-paste0('ID_',1:n)}
     if(class=='matrix'){
@@ -40,6 +57,7 @@ readGBS.AGCT<-function( fileIn, rsCol,allelesCol,nColSkip,header=TRUE,
                   nChunks=nChunks,dimorder=dimorder)
     }
     rownames(geno)<-GID
+
     fileIn<-gzfile(fileIn,open='r')
     map<-matrix(nrow=p,ncol=nColSkip,NA)
     if(header){ tmp<-scan(fileIn,nlines=1,quiet=TRUE,what=character())
@@ -48,18 +66,23 @@ readGBS.AGCT<-function( fileIn, rsCol,allelesCol,nColSkip,header=TRUE,
 
     for(i in 1:p){
         time<-proc.time()        
-        x<-scan(fileIn,nlines=1,what=character(),quiet=TRUE)
-        alleleOne<-unlist(strsplit(x[allelesCol],split='/'))[1]
-        map[i,]<-x[(1:nColSkip)]
-        x<-x[-(1:nColSkip)]
-        z<-rep(0,n)
-        z[x==alleleOne]<-2
-        z[x=='H']<-1
-        z[x==na.strings]<-NA
+        if(recode){
+	        x<-scan(fileIn,nlines=1,what=character(),quiet=TRUE,sep=sep,na.strings=na.strings)
+	        alleleOne<-unlist(strsplit(x[allelesCol],split='/'))[1]
+    	    map[i,]<-x[(1:nColSkip)]
+        	x<-x[-(1:nColSkip)]
+        	z<-rep(0,n)
+        	z[x==alleleOne]<-2
+        	z[x=='H']<-1            
+        }else{
+           map[i,]<-scan(fileIn,n=nColSkip,what=character(),quiet=TRUE,na.strings=na.strings,sep=sep)
+           z<-scan(fileIn,n=n,what=integer(),quiet=TRUE,na.strings=na.strings,sep=sep)
+        }
         geno[,i]<-z
+        
         if(verbose){
             cat('Marker ',i,' (of ',p, '),',round(proc.time()[3]-time[3],3),
-                 ' sec./marker.\n',sep='')
+                ' sec./marker.\n',sep='')
         }
     }
     close(fileIn)
@@ -82,6 +105,8 @@ readGBS.AGCT<-function( fileIn, rsCol,allelesCol,nColSkip,header=TRUE,
         attr(BGData,'dateCreated')<-date()
         save(BGData,file=paste(folderOut,'/BGData.RData',sep=''))
     }
+    
+    
     if(returnData){
         return(BGData)
     }
